9803e016155b7b5e018c52d523e21e0ccacc25ff
Adenike23 <adeofeadenike@gmail.com>
2023-09-24 17:56:51 +0100

    media query updated

--- js/main.js
+++ js/main.js
@@ -8,12 +8,12 @@ const  reset = document.querySelector('.reset');
 const textWarning = document.querySelector('.small')
 
 total.addEventListener('click', function() {
-    if (bill.value.length <= 0 || selectTip.value.length <= 0 || noOfPeople.value.length) {
-        bill.value.textContent = ''
-        selectTip.value.textContent = ''
-        noOfPeople.value.textContent = ''
-// } else if (noOfPeople.value === 0) {
-//     textWarning.style.display = 'block'
+    if (bill.value.length <= 0 || selectTip.value.length <= 0 ||
+         noOfPeople.value.length <= 0 || Number(noOfPeople.value) < 1) {
+        bill.value = ''
+        selectTip.value = ''
+        noOfPeople.value = ''
+            return
 }
 
 const a = (selectTip.value/100) * bill.value
@@ -22,6 +22,14 @@ tipAnswer.textContent = `${cal}`
 const cal2 = bill.value/noOfPeople.value;
 const cal3 = cal2 + cal;
 totalAnswer.textContent = `${cal3}`
+})
+
+reset.addEventListener('click', function() {
+    bill.value = '';
+    selectTip.value = '';
+    noOfPeople.value = '';
+    tipAnswer.textContent = '0.00';
+    totalAnswer.textContent = '0.00'
 })
 
 
